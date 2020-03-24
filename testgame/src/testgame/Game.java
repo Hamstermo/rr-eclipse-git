7c18dd21014beb59b60982b1facaf79ef6ba83a3
Hamstermo <joelhamlett3@gmail.com>
2020-03-24 18:02:50 +0000

    improved game loop

--- testgame/src/testgame/Game.java
+++ testgame/src/testgame/Game.java
@@ -70,9 +70,31 @@ public class Game implements Runnable, KeyListener {
 
 	@Override
 	public void run() {
-		while (true) {
-			tick();
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int updates = 0;
+		int frames = 0;
+		while(true) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1) {
+				tick();
+				updates++;
+				delta--;
+			}
 			render();
+			frames++;
+		   
+			if(System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println("FPS: " + frames + " Ticks: " + updates);
+				frames = 0;
+				updates = 0;
+			}
 		}
 	}
 
